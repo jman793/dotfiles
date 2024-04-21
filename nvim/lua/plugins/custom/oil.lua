6b3d8f8eb609b1d4adb15455c1302703d11a9572
jman793 <jmarz793@yahoo.com>
2024-04-21 13:52:03 -0700

    Change oil config

--- nvim/lua/plugins/custom/oil.lua
+++ nvim/lua/plugins/custom/oil.lua
@@ -1,5 +1,17 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
+  "stevearc/oil.nvim",
+  opts = {
+    columns = {
+      "icon",
+    },
+    default_file_explorer = true,
+    keymaps_help = {
+      border = "rounded",
+    },
+    use_default_keymaps = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
