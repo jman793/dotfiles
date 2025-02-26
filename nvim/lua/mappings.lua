e693c8f6ff0b3d86db9107e996e2ab5e3aab1394
jman793 <jmarz793@yahoo.com>
2025-02-25 22:22:05 -0800

    Add some more LSP keybinds

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,7 +11,10 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go To Definition" })
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go To Declaration" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go To Implementation" })
 vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { desc = "Go To Signature" })
+vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "Show References" })
 vim.keymap.set("n", "gE", go_to_error, { desc = "Go To Next Error" })
+vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 vim.keymap.set("i", "jk", "<ESC>")
 
 -- oil mappings
