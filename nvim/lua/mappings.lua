b438c1871866e678cff93259ba8b311f4e4e9799
jman793 <jmarz793@yahoo.com>
2025-03-21 00:28:45 -0700

    Add new mappings mostly for git

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,5 +1,3 @@
-require "nvchad.mappings"
-
 -- General Mappings
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -15,10 +13,24 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
+vim.keymap.set("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
+vim.keymap.set("n", "<leader>l", "<CMD>bnext<CR>")
+vim.keymap.set("n", "<leader>h", "<CMD>bprev<CR>")
+
+vim.keymap.set("n", "<leader>em", "<cmd>e ~/Repos/dotfiles/nvim/lua/mappings.lua<cr>")
 
 -- life changing
 vim.keymap.set("n", "<CR>", '@="m`o<C-V><Esc>``"<CR>')
 vim.keymap.set("n", "<S-CR>", '@="m`O<C-V><Esc>``"<CR>')
+
+vim.keymap.set("n", "<leader>th", function()
+  require("nvchad.themes").open()
+end, { desc = "telescope nvchad themes" })
+
+-- Which Key
+vim.keymap.set("n", "<leader>?", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 
 -- Lsp Mappings
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go To Definition" })
@@ -35,6 +47,7 @@ local go_to_error = function()
   vim.diagnostic.goto_next { severity = vim.diagnostic.severity.ERROR }
 end
 
+vim.keymap.set("n", "<leader>dq", vim.diagnostic.setqflist, { desc = "LSP diagnostic qflist" })
 vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "View Diagnostic" })
 vim.keymap.set("n", "gE", go_to_error, { desc = "Go To Next Error" })
 
@@ -46,11 +59,26 @@ vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>")
 
 -- Telescope Mappings
 local builtin = require "telescope.builtin"
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set(
+  "n",
+  "<leader>fa",
+  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  { desc = "Telescope find all files" }
+)
+vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope Live Grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope Search Buffers" })
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope Old Files" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope Help" })
+vim.keymap.set(
+  "n",
+  "<leader>fz",
+  "<cmd>Telescope current_buffer_fuzzy_find<CR>",
+  { desc = "telescope find in current buffer" }
+)
 
 -- Trouble Mappings
-vim.keymap.set("n", "<leader>tq", "<cmd>Trouble quickfix<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>tq", "<cmd>Trouble quickfix toggle<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>tx", "<cmd>Trouble diagnostics toggle<cr>")
 vim.keymap.set(
   "n",
@@ -83,3 +111,58 @@ end)
 
 -- Floating Terminal Mappings
 vim.keymap.set("n", "<leader>ft", "<cmd>FloatingTerminal<cr>i", { desc = "Toggle Floating Terminal" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+
+-- Gitsigns Mappings
+local gitsigns = require "gitsigns"
+
+-- Navigation
+vim.keymap.set("n", "]c", function()
+  if vim.wo.diff then
+    vim.cmd.normal { "]c", bang = true }
+  else
+    gitsigns.nav_hunk "next"
+  end
+end, { desc = "Next Git Hunk" })
+
+vim.keymap.set("n", "[c", function()
+  if vim.wo.diff then
+    vim.cmd.normal { "[c", bang = true }
+  else
+    gitsigns.nav_hunk "prev"
+  end
+end, { desc = "Prev Git Hunk" })
+
+-- Actions
+vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Git Stage Hunk" })
+vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Git Reset Hunk" })
+
+vim.keymap.set("v", "<leader>gs", function()
+  gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "Git Stage Highlighted" })
+
+vim.keymap.set("v", "<leader>gr", function()
+  gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "Git Reset Highlighted" })
+
+vim.keymap.set("n", "<leader>gus", gitsigns.undo_stage_hunk, { desc = "Git Undo Stage Hunk" })
+
+vim.keymap.set("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Git Stage Buffer" })
+vim.keymap.set("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Git Reset Buffer" })
+vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Git Preview Hunk" })
+vim.keymap.set("n", "<leader>gi", gitsigns.preview_hunk_inline, { desc = "Git Preview Hunk Inline" })
+
+vim.keymap.set("n", "<leader>gb", function()
+  gitsigns.blame_line { full = true }
+end, { desc = "Git Blame" })
+
+vim.keymap.set("n", "<leader>gd", gitsigns.diffthis, { desc = "Git Diff" })
+
+vim.keymap.set("n", "<leader>gQ", function()
+  gitsigns.setqflist "all"
+end)
+vim.keymap.set("n", "<leader>gq", gitsigns.setqflist, { desc = "Git Hunks Qflist" })
+
+-- Toggles
+vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Current Link Blame" })
+vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted)
